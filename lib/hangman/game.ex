574ca730db023adcc2ba17049cd031c4370c3d65
Mirco Poretti <hello@mircoporetti.me>
2026-05-14 13:17:27 +0200

    Guess a letter

--- lib/hangman/game.ex
+++ lib/hangman/game.ex
@@ -11,4 +11,17 @@ defmodule Hangman.Game do
       state: :playing
     }
   end
+
+  def guess(%__MODULE__{state: :playing} = game, letter) do
+    updated_guesses = MapSet.put(game.guesses, letter)
+    {:ok, %{game | guesses: updated_guesses}}
+  end
+
+  def wrong_guesses_count(%__MODULE__{word: word, guesses: guesses}) do
+    word_letters = word |> String.graphemes() |> MapSet.new()
+
+    guesses
+    |> Enum.reject(&MapSet.member?(word_letters, &1))
+    |> length()
+  end
 end
--- test/hangman/game_test.exs
+++ test/hangman/game_test.exs
@@ -13,4 +13,25 @@ defmodule Hangman.GameTest do
       assert game.state == :playing
     end
   end
+
+  describe "guess/2" do
+    test "the player can guess a letter that is in the word" do
+      game = Game.new("elixir")
+
+      {:ok, game} = Game.guess(game, "e")
+
+      assert MapSet.member?(game.guesses, "e")
+      assert game.state == :playing
+    end
+
+    test "a wrong guess counts as a mistake" do
+      game = Game.new("elixir")
+
+      {:ok, game} = Game.guess(game, "z")
+
+      assert MapSet.member?(game.guesses, "z")
+      assert Game.wrong_guesses_count(game) == 1
+      assert game.state == :playing
+    end
+  end
 end
